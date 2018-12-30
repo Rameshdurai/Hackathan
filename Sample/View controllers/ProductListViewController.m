1b4a6b0d3f72e7bc308955fbe4e0905644d1bc73
Ramesh Duraisamy <ramesh.duraisamy@aspiresys.com>
2018-12-30 13:16:39 +0530

    Added comments.

--- Sample/View controllers/ProductListViewController.m
+++ Sample/View controllers/ProductListViewController.m
@@ -13,50 +13,74 @@
 #import <AFNetworking/AFNetworking.h>
 #import "UIImageView+AFNetworking.h"
 
-
 @interface ProductListViewController ()
 
 @end
 
 @implementation ProductListViewController
 
-- (void)viewDidLoad {
+- ( void )viewDidLoad
+{
     [super viewDidLoad];
 
+    // Set background color and frame for tableview.
     UIGraphicsBeginImageContext(self.view.frame.size);
     [[UIImage imageNamed:@"Bg.png"] drawInRect:self.view.bounds];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
     self.tableView.backgroundColor = [UIColor colorWithPatternImage:image];
-    
     [self.tableView setFrame:CGRectMake(0,0, self.view.frame.size.width, self.view.frame.size.height)];
-    
-    self.activityIndicator = [[SFActivityIndicator alloc]init];
-    
 
-    // Do any additional setup after loading the view.
+    // Intilaize activity indicator.
+    self.activityIndicator = [[SFActivityIndicator alloc]init];
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+/*!
+ Set number of table view sections.
+
+ @param tableView Table view instance.
+ @return Number of sections.
+ */
+- ( NSInteger )numberOfSectionsInTableView:( UITableView * )tableView
 {
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+/*!
+ Set number of rows in particular section.
+
+ @param tableView Table view.
+ @param section Section index.
+ @return Number of rows in particular section.
+ */
+- ( NSInteger )tableView:( UITableView * )tableView
+   numberOfRowsInSection:( NSInteger )section
 {
     return self.products.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+/*!
+ Create product table view cell.
+ 
+ @param tableView Table view.
+ @param indexPath Index path of the cell.
+ @return Product table view cell.
+ */
+- ( UITableViewCell * )tableView:( UITableView * )tableView
+           cellForRowAtIndexPath:( NSIndexPath * )indexPath
 {
+    // Cell identifier.
     static NSString *CellIdentifier = @"ProductCell";
 
+    // Create product table view cell.
     ProductTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: CellIdentifier forIndexPath: indexPath];
 
+    // Get product informations.
     Product *product = [self.products objectAtIndex:indexPath.row];
+
+    // Set values to product table view cell.
     cell.productName.text = product.productName;
     cell.ProductBrandName.text = product.productBrandName;
     cell.productCategoryName.text = product.productColor;
@@ -65,6 +89,7 @@
     cell.productImageView.contentMode = UIViewContentModeScaleAspectFit;
     cell.productImageView.image = [UIImage imageNamed:product.productImage];
 
+    // TODO : Lazzy loading.
 //    NSURL *url = [NSURL URLWithString:product.productImage];
 //    NSURLRequest *request = [NSURLRequest requestWithURL:url];
 //    UIImage *placeholderImage = [UIImage imageNamed:@"Sample 2_0.jpg"];
@@ -78,40 +103,29 @@
 //                                       weakCell.productImageView.image = image;
 //
 //                                   } failure:nil];
-    
     return cell;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+- ( CGFloat )tableView:( UITableView * )tableView
+heightForRowAtIndexPath:( NSIndexPath * )indexPath
 {
     return 150.0f;
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-- (IBAction)searchButtonHanduler:(id)sender {
-    
+- ( IBAction )searchButtonHanduler:( id )sender
+{
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width-10, 44.0)];
     
     _searchBar = [[UISearchBar alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width-10, 44.0)];
     _searchBar.delegate = self;
     [view addSubview:_searchBar];
-    
+
     [_searchBar becomeFirstResponder];
 
     self.navigationItem.titleView = view;
 }
 
-- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+- ( void )searchBarSearchButtonClicked:( UISearchBar * )searchBar
 {
     [searchBar resignFirstResponder];
     [_searchBar removeFromSuperview];
